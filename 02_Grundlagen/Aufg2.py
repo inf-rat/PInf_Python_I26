be6f41b68f90a4d8e5fbbb518b04710b7f41bd8c
inf-rat <r.ratke@schule.bremen.de>
2026-08-25 10:45:32 +0200

    Aufg. 2a

---
+++ 02_Grundlagen/Aufg2.py
@@ -0,0 +1,9 @@
+# 2a
+name = input("Bitte deinen Namen eingeben: ")
+print(f"Guten Tag, {name}!")
+
+# Hilfestellung zu 2b
+teststring = "PYTHON_IST_KEINE_INSEL"
+print(teststring[0:3])
+print(teststring[3:6])
+print(teststring[6:9])
