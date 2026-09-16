5ef41a71b92aeb849060d176acf50b9469ab6f18
inf-rat <r.ratke@schule.bremen.de>
2026-09-16 11:40:31 +0200

    AB 04 komplett

--- 04_Rekursion/Aufg4.py
+++ 04_Rekursion/Aufg4.py
@@ -1,21 +1,20 @@
-import turtle
-from tkinter import mainloop
+from turtle import *
 
 
 def koch_curve(steps: int, dist: float):
     if steps <= 1:
-        turtle.forward(dist)
+        forward(dist)
     else:
         dist /= 3.0
         koch_curve(steps - 1, dist)
-        turtle.left(60)
+        left(60)
         koch_curve(steps - 1, dist)
-        turtle.right(120)
+        right(120)
         koch_curve(steps - 1, dist)
-        turtle.left(60)
+        left(60)
         koch_curve(steps - 1, dist)
-        turtle.speed(0)
+        speed(0)
 
-turtle.speed(0)
+speed(0)
 koch_curve(5,300)
 mainloop()
